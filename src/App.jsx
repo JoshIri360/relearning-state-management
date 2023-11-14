328a3fffaef22b30f6aab2866efbad25914f2c24
Joshua Aideloje <aidelojejoshua@gmail.com>
2023-11-14 16:48:52 +0100

    useMemo tutorial complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { useState, useMemo } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  // Use useMemo when you have a potentially expensive operation
+  const [numbers] = useState([10, 20, 30]);
+
+  const total = useMemo(
+    () => numbers.reduce((acc, curr) => acc + curr, 0),
+    [numbers]
+  );
+
+  return <div>Total {total}</div>;
 };
 
 export default App;
