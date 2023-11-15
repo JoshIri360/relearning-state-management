be7a0940ed46c9a15d9f972b9c9cc40aa0e2f891
Joshua Aideloje <aidelojejoshua@gmail.com>
2023-11-15 11:59:32 +0100

    Completed useRef tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import { useRef, useEffect, useState } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const idRef = useRef(1);
+
+  const [names, setNames] = useState([
+    { name: "John", id: idRef.current++ },
+    { name: "Mary", id: idRef.current++ },
+  ]);
+
+  const addName = () => {
+    const name = inputRef.current.value;
+    setNames([...names, { name, id: idRef.current++ }]);
+    inputRef.current.value = "";
+  };
+
+  return (
+    <div>
+      Input: <input ref={inputRef} type="text" />
+      <button
+        onClick={() => {
+          addName();
+        }}
+      >
+        Add name
+      </button>
+      <ul>
+        {names.map((name) => (
+          <li key={name.id}>{`${name.id} ${name.name}`}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
